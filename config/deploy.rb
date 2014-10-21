92ad944feaa2cfa53bb09ad1c611bdcc41fd69ac
den <den@ubuntu.(none)>
2014-10-21 12:30:02 +1100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,21 @@ set :rails_env, 'production'
 set :rvm_ruby_version, '2.1.3'
 set :deploy_to, '/home/deployer/apps/storage'
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, true
+set :puma_prune_bundler, false
 
 namespace :foreman do
   desc 'Start server'
@@ -61,6 +76,10 @@ namespace :deploy do
       execute "mkdir  /home/deployer/apps/#{application}/log/"
       execute "mkdir  /home/deployer/apps/#{application}/socket/"
       execute "mkdir #{shared_path}/system"
+      execute "mkdir #{shared_path}/log"
+      execute "mkdir #{shared_path}/tmp"
+      execute "mkdir #{shared_path}/tmp/pids"
+      execute "mkdir #{shared_path}/tmp/sockets"
       execute "mkdir /home/deployer/log/"
       execute "mkdir /home/deployer/uploads/"
       sudo "ln -s /var/log/upstart /home/deployer/log/upstart"
