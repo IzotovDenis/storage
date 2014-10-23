5fd62f393ab5f62ebc9c5bd168839862eb18a5fd
den <den@ubuntu.(none)>
2014-10-23 11:07:24 +1100

    devise fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  authenticated :user do
-    root 'folders#index', :as => :authenticated_root
-  end
-  root :to => redirect('/users/sign_in')
-
   resources :folders
   resources :docs
 
