d958c50685edf3e7ce0abf0db252d43340dc481c
den <den@ubuntu.(none)>
2014-10-28 11:10:58 +1100

    remember password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def remember_me
+  	true
+  end
+   
 end
