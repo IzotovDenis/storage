30069f2f73ad9d96979aee0a411a2f162df13a1c
den <den@ubuntu.(none)>
2014-11-24 09:07:59 +1000

    storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider               => 'OpenStack',
         :openstack_auth_url     => 'https://auth.selcdn.ru/v1.0',
-        :openstack_username     => "37947",
-        :openstack_api_key      => "OY9r8s8U"
+        :openstack_username     => "#{CONFIG[:fog_login]}",
+        :openstack_api_key      => "#{CONFIG[:fog_key]}"
     }
     config.fog_directory  = "storage"  # required
     config.fog_public     = true                                 # optional, defaults to true
