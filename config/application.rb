86d8e8543ffb6e58433f13d9b0806da4f37aa889
den <den@ubuntu.(none)>
2014-11-24 09:11:00 +1000

    CONFIG application.yml

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ require 'carrierwave'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.symbolize_keys!
+
 module Storage
   class Application < Rails::Application
     CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
